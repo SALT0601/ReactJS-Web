d09505290f8f4f3975092446231426ab8ce10d15
SALT0601 <thfxm0601@gmail.com>
2021-11-26 19:54:50 +0900

    #5.1 Tour of CRA

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div><h1>Welcome back!</h1></div>
+    <div>
+      <h1>Welcome back!!!</h1>
+      <h1 className={styles.title}>Welcome back!!!</h1>
+      <Button text={"Continue"} />
+    </div>
   );
 }
-
 export default App;
