976728b179b5b3db8b3adc0488f05105b9bd8901
SALT0601 <thfxm0601@gmail.com>
2021-11-28 21:39:17 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('render');
+  console.log("i run all the time");
+  
+  useEffect(() => {
+    console.log("CALL THE API")
+  },[]);
   return (
     <div>
       
