7a6867224610d83ecfe6aa05630c0769ceccfde9
SALT0601 <thfxm0601@gmail.com>
2021-12-03 02:50:14 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import { useState, useEffect } from "react";
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, []);
+  /* useEffect(function () {
+    console.log("created :)");
+    return function (){console.log("destroyed :(");};
+  }, []);-> 다른방법인데 너무 길어서 보통 위의 방법을 씀 */
+  return <h1>Hello</h1>; 
+}
 
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => setKeyword(event.target.value);
-
-  const onClick = () => setValue((prev) => prev + 1);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   
-  
-  useEffect(() => {
-    console.log("I run only once. ");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.")
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.")
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword & counter' changes.")
-  }, [keyword, counter]);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+     <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
