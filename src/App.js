0236b25d3a31a1d3ed80644ccb965bb25ab34fb9
SALT0601 <thfxm0601@gmail.com>
2021-12-04 02:47:57 +0900

    #7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import { useState, useEffect } from "react";
-function Hello() {
-  useEffect(() => {
-    console.log("created :)");
-    return () => console.log("destroyed :(");
-  }, []);
-  /* useEffect(function () {
-    console.log("created :)");
-    return function (){console.log("destroyed :(");};
-  }, []);-> 다른방법인데 너무 길어서 보통 위의 방법을 씀 */
-  return <h1>Hello</h1>; 
-}
+import { useState } from "react";
 
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]); 
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos(currentArray => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
+  
+  
   
   return (
     <div>
-      {showing ? <Hello /> : null}
-     <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+     <form onSubmit={onSubmit}> 
+       <input 
+       onChange={onChange} 
+       value={toDo} 
+       type="text" 
+       placeholder="Write your to do..." 
+       />
+       <button>Add To Do</button>
+     </form>
     </div>
   );
 }
