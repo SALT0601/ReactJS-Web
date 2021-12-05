ebce52caaf223783d9c230426f062e7b92bc182f
SALT0601 <thfxm0601@gmail.com>
2021-12-05 17:17:23 +0900

    #7.2 Coin Tracker_Challenge

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect } from "react/cjs/react.development";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState([]);
  
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers").then((response) =>
@@ -13,14 +14,24 @@ function App() {
       setLoading(false);
     });
   }, []);
- 
+  function onChange(event) {
+    setMyMoney(event.target.value)
+
+  }
   
   
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <input 
+        onChange={onChange}
+        value={myMoney}
+        type="number"
+        placeholder="Please Write your USD"
+      />
       {loading ? <strong>Loading...</strong> : <select>
-        {coins.map((coin) => <option>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
+        {coins.map((coin) => <option>{coin.name} ({coin.symbol}): 
+        {myMoney / coin.quotes.USD.price} {coin.symbol}</option>)}
       </select> }
       
     </div>
