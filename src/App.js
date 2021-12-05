2ade0a8388336833c3fe366bf9440c0fb8c737eb
SALT0601 <thfxm0601@gmail.com>
2021-12-05 14:08:27 +0900

    #7.1 To Do List part Two

--- src/App.js
+++ src/App.js
@@ -29,6 +29,10 @@ function App() {
        />
        <button>Add To Do</button>
      </form>
+     <hr />
+      <ul>
+      {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
